6f4dd02baf0f50a42653b5b329a105b10aeecfc2
ErhaRafiq <irhaakhawaja@gmail.com>
2026-05-08 15:34:25 -0700

    Update test_ecommerce.py

--- test_ecommerce.py
+++ test_ecommerce.py
@@ -1,26 +1,33 @@
 import unittest
 from selenium import webdriver
-from webdriver_manager.chrome import ChromeDriverManager
 from selenium.webdriver.common.by import By
 from selenium.webdriver.chrome.options import Options
 from selenium.webdriver.chrome.service import Service
 import time
+import os
+import shutil
 
 class EcommerceTests(unittest.TestCase):
     @classmethod
-    def setUpClass(cls):
-        # Set up Chrome options for headless mode
-        chrome_options = Options()
-        chrome_options.add_argument("--headless")
-        chrome_options.add_argument("--disable-gpu")
-        chrome_options.add_argument("--no-sandbox")
-        chrome_options.add_argument("--disable-dev-shm-usage")
-        chrome_options.add_argument("--window-size=1920,1080")
+def setUpClass(cls):
+    chrome_options = Options()
+    chrome_options.add_argument("--headless")
+    chrome_options.add_argument("--disable-gpu")
+    chrome_options.add_argument("--no-sandbox")
+    chrome_options.add_argument("--disable-dev-shm-usage")
+    chrome_options.add_argument("--window-size=1920,1080")
+    chrome_options.add_argument("--remote-debugging-port=9222")
 
-        # Automatically handle the download of the correct ChromeDriver
-        cls.driver = webdriver.Chrome(service=Service(ChromeDriverManager().install()), options=chrome_options)
-        cls.driver.implicitly_wait(5)
-        cls.base_url = "http://127.0.0.1:5000"
+    chromedriver_path = shutil.which("chromedriver") or "/usr/local/bin/chromedriver"
+
+    cls.driver = webdriver.Chrome(
+        service=Service(chromedriver_path),
+        options=chrome_options
+    )
+
+    cls.driver.implicitly_wait(5)
+    cls.base_url = os.environ.get("APP_URL", "http://127.0.0.1:5000")
+        
 
     @classmethod
     def tearDownClass(cls):
